e46aa1cd4cdff9c324f41385d9c78cbbf0ba0100
LuaMaster13 <81645287+LuaMaster13@users.noreply.github.com>
2021-03-30 14:51:36 -0700

    Create Loops.lua

---
+++ Loops.lua
@@ -0,0 +1,17 @@
+--Loops
+
+while true do
+  wait(0)
+  test()
+end
+
+--[[ Explanation
+
+while true do  The begining of the loop
+  wait(0)      This is required so Roblox and/or Roblox Studio won't crash, but you can change this to anything
+  test()       To call the function
+end            The end of the loop
+
+You can change the middle of the loop to anything
+
+]]
